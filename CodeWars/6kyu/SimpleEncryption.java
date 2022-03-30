07c2406796770ef978248ec387ddcbc96986e501
DRMPN <mr.gorden.smit@gmail.com>
2022-03-30 23:00:04 +0300

    added 6kye problem

--- CodeWars/6kyu/SimpleEncryption.java
+++ CodeWars/6kyu/SimpleEncryption.java
@@ -1,36 +1,54 @@
 public class Kata {
 
-    public static String encrypt(final String text, final int n) {
-      
-      if (text == "") return "";
-      if (text == null) return null;
-      
-      var txt = text;
-      var times = n;
-      
-      while (times > 0) {
-          String odd = "";
-          String even = "";
-        
-          for (int i = 0; i < txt.length(); i++) {
-              if (i % 2 == 0) {
-                  even += txt.charAt(i);
-              } else {
-                  odd += txt.charAt(i);
-              }
-          }
-          txt = odd + even;
-          times--;
-      }
-      
-      return txt;   
-    }
-    
-    public static String decrypt(final String encryptedText, final int n) {
-      if (encryptedText == "") return "";
-      if (encryptedText == null) return null;
-      
+  public static String encrypt(final String text, final int n) {
+
+    if (text == null)
       return null;
+
+    var txt = text;
+
+    for (int jk = n; jk > 0; jk--) {
+      var odd = "";
+      var even = "";
+
+      for (int i = 0; i < txt.length(); i++) {
+        if (i % 2 == 0) {
+          even += txt.charAt(i);
+        } else {
+          odd += txt.charAt(i);
+        }
+      }
+      txt = odd + even;
     }
-    
+
+    return txt;
   }
+
+  public static String decrypt(final String encryptedText, final int n) {
+
+    if (encryptedText == null)
+      return null;
+
+    var txt = encryptedText;
+
+    var txtLen = txt.length();
+    var evenStart = txtLen / 2;
+
+    for (int jk = n; jk > 0; jk--) {
+
+      var tmp = "";
+
+      for (int i = 0; i < evenStart; i++) {
+        tmp += txt.charAt(i + evenStart);
+        tmp += txt.charAt(i);
+      }
+      if (txtLen % 2 != 0)
+        tmp += txt.charAt(txtLen - 1);
+
+      txt = tmp;
+    }
+
+    return txt;
+  }
+
+}
