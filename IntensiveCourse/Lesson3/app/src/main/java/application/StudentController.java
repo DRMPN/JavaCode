78053c2f35938904188cde0ae8310ae9e6f3dfc8
DRMPN <mr.gorden.smit@gmail.com>
2022-06-23 13:38:14 +0300

    added servlet path

--- IntensiveCourse/Lesson3/app/src/main/java/application/StudentController.java
+++ IntensiveCourse/Lesson3/app/src/main/java/application/StudentController.java
@@ -7,10 +7,12 @@ import java.util.Optional;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/")
 public class StudentController extends HttpServlet{
     private DaoStudent studentDao = DaoStudent.getInstance();
 
