2a0d250dbc6a5acb9960ef55df98040b3cf66d1d
DRMPN <mr.gorden.smit@gmail.com>
2022-06-25 12:42:59 +0300

    changed servlet route

--- IntensiveCourse/Lesson4/app/src/main/java/crud/servlets/IndexServlet.java
+++ IntensiveCourse/Lesson4/app/src/main/java/crud/servlets/IndexServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet("/app")
 public class IndexServlet extends HttpServlet{
 
     @Override
