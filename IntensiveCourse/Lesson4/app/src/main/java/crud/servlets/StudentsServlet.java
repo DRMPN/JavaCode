e636788cb51d9799dc078b88bef9a174b454f722
DRMPN <mr.gorden.smit@gmail.com>
2022-06-25 12:33:34 +0300

    edited links

--- IntensiveCourse/Lesson4/app/src/main/java/crud/servlets/StudentsServlet.java
+++ IntensiveCourse/Lesson4/app/src/main/java/crud/servlets/StudentsServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/students")
+@WebServlet("/app/students")
 public class StudentsServlet extends HttpServlet{
 
     @Override
