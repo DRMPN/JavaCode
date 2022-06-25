d7a450975e2032c644c9e0b521aab74c3974a87a
DRMPN <mr.gorden.smit@gmail.com>
2022-06-25 18:52:54 +0300

    test fix

--- IntensiveCourse/Lesson4/app/src/main/java/crud/servlets/StudentsServlet.java
+++ IntensiveCourse/Lesson4/app/src/main/java/crud/servlets/StudentsServlet.java
@@ -14,7 +14,7 @@ import crud.model.Student;
 import crud.repo.StudentsRepo;
 
 @WebServlet("/students")
-public class StudentsServlet extends HttpServlet{
+public class StudentsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -22,8 +22,8 @@ public class StudentsServlet extends HttpServlet{
         Student student = null;
         List<Student> students = StudentsRepo.listStudents();
 
-        int editid = (req.getParameter("editId")==null) ? 0 : Integer.parseInt(req.getParameter("editId"));
-        int delId = (req.getParameter("delId")==null ? 0 : Integer.parseInt(req.getParameter("delId")));
+        int editid = (req.getParameter("editId") == null) ? 0 : Integer.parseInt(req.getParameter("editId"));
+        int delId = (req.getParameter("delId") == null ? 0 : Integer.parseInt(req.getParameter("delId")));
 
         if (editid > 0) {
             // TODO: update student
@@ -39,18 +39,15 @@ public class StudentsServlet extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-        String firstName = req.getParameter("firstName");
-        String lastName = req.getParameter("lastName");
-        int age = Integer.parseInt(req.getParameter("age"));
-        String program = req.getParameter("program");
 
-        Student student = new Student(firstName, lastName, age, program);
+        Student student = new Student(req.getParameter("firstName"),
+                req.getParameter("lastName"),
+                Integer.parseInt(req.getParameter("age")),
+                req.getParameter("program"));
 
         StudentsRepo.addStudent(student);
 
         doGet(req, resp);
     }
-    
-    
+
 }
