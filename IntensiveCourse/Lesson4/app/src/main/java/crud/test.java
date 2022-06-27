57dc0c07324e1b001cf08d21eccb0be8949dfb5f
DRMPN <mr.gorden.smit@gmail.com>
2022-06-27 13:58:51 +0300

    added polymorphic queries

--- IntensiveCourse/Lesson4/app/src/main/java/crud/test.java
+++ IntensiveCourse/Lesson4/app/src/main/java/crud/test.java
@@ -1,8 +1,12 @@
 package crud;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import crud.model.AbstractClass;
+import crud.model.Class;
 import crud.model.LocationBasedClass;
 import crud.model.PaidClass;
 import crud.model.PartnershipClass;
@@ -14,8 +18,12 @@ public class test {
         Session session = HibernateDataSource.getSession();
         Transaction transaction = session.beginTransaction();
 
-        session.createQuery("delete from AbstractClass").executeUpdate();
-
+        //session.createQuery("delete from AbstractClass").executeUpdate();   
+        
+        Class c = new Class();
+        c.setTitle("Computer Science 101");
+        c.setDescription("General class");
+        
         LocationBasedClass lbc = new LocationBasedClass();
         lbc.setTitle("LBC Title");
         lbc.setDescription("LBC Description");
@@ -31,9 +39,27 @@ public class test {
         pc.setDescription("PC Description");
         pc.setPrice(9.99);
 
-        session.save(lbc);
-        session.save(psc);
-        session.save(pc);
+        //session.save(c);
+        //session.save(lbc);
+        //session.save(psc);
+        //session.save(pc);
+
+        List<AbstractClass> list = session.createQuery("FROM AbstractClass").list();
+        List<AbstractClass> list0 = session.createQuery("FROM Class").list();
+        List<AbstractClass> list1 = session.createQuery("FROM LocationBasedClass").list();
+        List<AbstractClass> list2 = session.createQuery("FROM PartnershipClass").list();
+        List<AbstractClass> list3 = session.createQuery("FROM PaidClass").list();
+
+        System.out.println("===========================================");
+        list.forEach(System.out::println);
+        System.out.println("===========================================");
+        list0.forEach(System.out::println);
+        System.out.println("===========================================");
+        list1.forEach(System.out::println);
+        System.out.println("===========================================");
+        list2.forEach(System.out::println);
+        System.out.println("===========================================");
+        list3.forEach(System.out::println);
         
         transaction.commit();
         session.close();
